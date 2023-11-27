b954ed9ff8e16636e320b641b0996f546525cb25
Anubhav Chaudhary <126059931+13anubhav@users.noreply.github.com>
2023-11-27 20:07:54 +0530

    Update [id].js --> 27-11-23 --> 8.07 PM

--- src/pages/job/[id].js
+++ src/pages/job/[id].js
@@ -1,3 +1,4 @@
+// pages/job/[id].js
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import ApplicationForm from '../form/f3';
@@ -19,35 +20,68 @@ const JobDetail = () => {
   };
 
   return (
-    <div>
+    <div className="container">
       <h1>Job Details</h1>
-      <p>Job ID: {id}</p>
-      <p>Title: {parsedJobDetails.title}</p>
-      <p>Description: {parsedJobDetails.description}</p>
-      <p>Max Salary: {parsedJobDetails.salary_max}</p>
-      <p>Min Salary: {parsedJobDetails.salary_min}</p>
+      <div className="details-section">
+        <p className="detail"><strong>Job ID:</strong> {id}</p>
+        <p className="detail"><strong>Title:</strong> {parsedJobDetails.title}</p>
+        <p className="detail"><strong>Description:</strong> {parsedJobDetails.description}</p>
+        <p className="detail"><strong>Max Salary:</strong> {parsedJobDetails.salary_max}</p>
+        <p className="detail"><strong>Min Salary:</strong> {parsedJobDetails.salary_min}</p>
+      </div>
+
+      <div className="details-section">
+        <p className="detail"><strong>Category:</strong> {parsedJobDetails.category.label}</p>
+        <p className="detail"><strong>Job Type:</strong> {parsedJobDetails.contract_type}</p>
+        <p className="detail"><strong>Location:</strong> {parsedJobDetails.location.area}</p>
+        <p className="detail"><strong>Location 2:</strong> {parsedJobDetails.location.display_name}</p>
+        <p className="detail"><strong>Job Posted Time:</strong> {parsedJobDetails.created}</p>
+      </div>
+
       {/* Add more details as needed */}
 
-      <button onClick={handleApplyClick}>Apply</button>
+      <button onClick={handleApplyClick}>Apply Now</button>
+
+<br />
+<h1></h1>
 
       {showApplicationForm && <ApplicationForm />}
 
       <style jsx>{`
-        div {
+        .container {
           max-width: 800px;
           margin: 0 auto;
           padding: 20px;
+          background-color: #f9f9f9;
+          border: 1px solid #ddd;
+          border-radius: 5px;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
 
         h1 {
           color: #333;
           font-size: 32px;
           margin-bottom: 20px;
+          text-align: center;
+        }
+
+        .details-section {
+          margin-bottom: 20px;
+        }
+
+        .detail {
+          color: #444;
+          margin-bottom: 10px;
+          font-size: 16px;
+        }
+
+        strong {
+          font-weight: bold;
         }
 
         button {
           font-size: 16px;
-          padding: 8px;
+          padding: 10px;
           background-color: #4caf50;
           color: white;
           border: none;
@@ -57,42 +91,6 @@ const JobDetail = () => {
 
         button:hover {
           background-color: #45a049;
-        }
-
-        label {
-          display: block;
-          margin-bottom: 10px;
-        }
-
-        select {
-          font-size: 16px;
-          padding: 8px;
-        }
-
-        ul {
-          list-style: none;
-          padding: 0;
-        }
-
-        .job-item {
-          border: 1px solid #ddd;
-          margin: 10px 0;
-          padding: 10px;
-          border-radius: 5px;
-        }
-
-        h3 {
-          color: #0070f3;
-        }
-
-        p {
-          color: #555;
-        }
-
-        .job-link {
-          color: #0070f3;
-          text-decoration: underline;
-          cursor: pointer;
         }
       `}</style>
     </div>
