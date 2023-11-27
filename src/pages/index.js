297d44d006d95e69c973384b217776945b068987
Anubhav Chaudhary <126059931+13anubhav@users.noreply.github.com>
2023-11-27 20:06:38 +0530

    Update index.js --> 27-11-23 -->8.06 PM

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,17 @@
 // pages/index.js
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 
 const Home = () => {
   const [jobs, setJobs] = useState([]);
   const [selectedLanguage, setSelectedLanguage] = useState('javascript'); // Default value
+  const [showScrollButton, setShowScrollButton] = useState(false);
 
   const fetchData = async () => {
     try {
-      const response = await fetch(
-        `https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=9138079d&app_key=ae558c63fea85f27e8293e6c94f4b53f&what=${selectedLanguage}&results_per_page=15`
-      );
-
-      if (!response.ok) {
-        throw new Error(`Adzuna API error! Status: ${response.status}`);
-      }
-
-      const data = await response.json();
-      setJobs(data.results);
+      const response = await axios.get(`/api/adzuna1?what=${selectedLanguage}`);
+      setJobs(response.data.results);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -26,7 +19,19 @@ const Home = () => {
 
   useEffect(() => {
     fetchData();
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, [selectedLanguage]);
+
+  const handleScroll = () => {
+    if (window.scrollY > 300) {
+      setShowScrollButton(true);
+    } else {
+      setShowScrollButton(false);
+    }
+  };
 
   const handleLanguageChange = (e) => {
     setSelectedLanguage(e.target.value);
@@ -36,13 +41,17 @@ const Home = () => {
     fetchData();
   };
 
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   const shortenUrl = (url) => {
     return url.length > 30 ? url.substring(0, 30) + '...' : url;
   };
 
   return (
-    <div>
-      <h1>Adzuna Jobs</h1>
+    <div className="container">
+      <h1>Anubhav Job Search</h1>
       <label>
         Select Language:
         <select value={selectedLanguage} onChange={handleLanguageChange}>
@@ -50,48 +59,59 @@ const Home = () => {
           <option value="javascript">JavaScript</option>
           <option value="python">Python</option>
           <option value="java">Java</option>
+          <option value="frontend">Frontend</option>
+          <option value="backend">Backend</option>
+          <option value="nodeJs">NodeJs</option>
+          <option value="datascience">Data Science</option>
         </select>
       </label>
       <button onClick={handleSubmit}>Search Jobs</button>
       <ul>
-      {jobs.map((job) => (
+        {jobs.map((job) => (
           <li key={job.id} className="job-item">
             <h3>{job.title}</h3>
-            <p>{job.description}</p>
-            <p>Max Salary: {job.salary_max}</p>
-            <p>Min Salary: {job.salary_min}</p>
-            <p>Category: {job.category.label}</p>
+            {/* <p>{job.description}</p> */}
+            <p>Salary: {job.salary_max}</p>
+            {/* <p>Min Salary: {job.salary_min}</p> */}
+            {/* <p>Category: {job.category.label}</p> */}
+            <p>Job Posted Time: {job.created}</p>
             <p>Location: {job.location.area}</p>
             <p>Company Name: {job.company.display_name}</p>
-            <p>URL: {shortenUrl(job.redirect_url)}</p>
-            {/* http://localhost:3000/job/ */}
-
-            <Link href={{ pathname: '/job/[id]', query: { id: job.id, jobDetails: JSON.stringify(job) } }}>
-            
-            {/* <Link href={{ pathname: 'https://anubhav-job-portal.netlify.app/job/[id]', query: { id: job.id, jobDetails: JSON.stringify(job) } }}> */}
-
-
+            <p>Job ID: {job.id}</p>
+            {/* <p>URL: {shortenUrl(job.redirect_url)}</p> */}
+            <Link href={{ pathname: '/job/[id]', query: { id: job.id, jobDetails: JSON.stringify(job) } }} target="_blank">
               <p className="job-link">View Full Details</p>
             </Link>
-            <p>Job id : {job.id}</p>
-            <Link href={`/form/f3`}>
+            {/* <p>Job ID: {job.id}</p> */}
+            {/* <Link href={`/form/f3`}>
               <p className="job-link">Apply For this Job</p>
-            </Link>
+            </Link> */}
           </li>
         ))}
       </ul>
 
+      {showScrollButton && (
+        <div className="scroll-button" onClick={scrollToTop}>
+          &uarr; Scroll to Top
+        </div>
+      )}
+
       <style jsx>{`
-        div {
+        .container {
           max-width: 800px;
           margin: 0 auto;
           padding: 20px;
+          background-color: #f9f9f9;
+          border: 1px solid #ddd;
+          border-radius: 10px;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
 
         h1 {
           color: #333;
           font-size: 32px;
           margin-bottom: 20px;
+          text-align: center;
         }
 
         label {
@@ -99,9 +119,23 @@ const Home = () => {
           margin-bottom: 10px;
         }
 
-        select, button {
+        select,
+        button {
           font-size: 16px;
           padding: 8px;
+          margin-bottom: 20px;
+        }
+
+        button {
+          background-color: #4caf50;
+          color: white;
+          border: none;
+          border-radius: 5px;
+          cursor: pointer;
+        }
+
+        button:hover {
+          background-color: #45a049;
         }
 
         ul {
@@ -111,22 +145,40 @@ const Home = () => {
 
         .job-item {
           border: 1px solid #ddd;
-          margin: 10px 0;
-          padding: 10px;
-          border-radius: 5px;
+          margin: 20px 0;
+          padding: 20px;
+          border-radius: 10px;
+          transition: box-shadow 0.3s ease-in-out;
+        }
+
+        .job-item:hover {
+          box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
         }
 
         h3 {
           color: #0070f3;
+          margin-bottom: 10px;
         }
 
         p {
           color: #555;
+          margin-bottom: 10px;
         }
 
         .job-link {
           color: #0070f3;
           text-decoration: underline;
+          cursor: pointer;
+        }
+
+        .scroll-button {
+          position: fixed;
+          bottom: 20px;
+          right: 20px;
+          background-color: #0070f3;
+          color: white;
+          padding: 10px 20px;
+          border-radius: 5px;
           cursor: pointer;
         }
       `}</style>
@@ -135,33 +187,3 @@ const Home = () => {
 };
 
 export default Home;
-
-// // pages/api/adzuna.js
-// export default async function handler(req, res) {
-//   try {
-//     const { what } = req.query;
-
-//     const response = await fetch('https://api.adzuna.com/v1/api/jobs/gb/search/1', {
-//       method: 'GET',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       qs: {
-//         app_id: '9138079d',
-//         app_key: 'ae558c63fea85f27e8293e6c94f4b53f',
-//         what, // Use the selected language passed from the frontend
-//         results_per_page: 15,
-//       },
-//     });
-
-//     if (!response.ok) {
-//       throw new Error(`Adzuna API error! Status: ${response.status}`);
-//     }
-
-//     const data = await response.json();
-//     res.status(200).json(data);
-//   } catch (error) {
-//     console.error('Adzuna API error:', error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// }
